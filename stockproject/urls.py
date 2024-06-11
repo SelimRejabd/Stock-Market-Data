9b36325da98e8940cdc58a384c93ee75c390d6a8
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-06-11 20:10:30 +0600

    sqlModel & CRUD applications

--- stockproject/urls.py
+++ stockproject/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('stockdata.urls')),
+    path('api/stocks-data/', include('stock_database.urls'))
 ]
